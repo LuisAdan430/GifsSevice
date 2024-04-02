82c83044f8e7698cde46319a83c781e383034bb4
LuisAdan430 <luisangelcanalescalva@gmail.com>
2024-04-02 17:37:29 -0600

    Creacion de las interfaces con la herramienta quicktype

--- gifs-app/src/app/gifs/services/gifs.service.ts
+++ gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,12 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
+
+  public gifList: Gif[] = [];
 
   private apiKey:string = 'K77knKxTMbWDSTCpc4enPVPEaSh50WzS';
   private serviceUrl:string = 'https://api.giphy.com/v1/gifs';
@@ -41,10 +44,10 @@ export class GifsService {
     .set('limit', '10')
     .set('q', tag);
 
-    this.http.get(`${this.serviceUrl}/search`, { params })
-    .subscribe( resp => {
-      console.log(resp);
-
+    this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, { params })
+    .subscribe(resp => {
+      this.gifList = resp.data;
+      console.log({gifs: this.gifList});
     });
   }
 
