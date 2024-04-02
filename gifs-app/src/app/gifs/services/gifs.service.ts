4fcec276b776e2e36e0713693369413efcf14d62
LuisAdan430 <luisangelcanalescalva@gmail.com>
2024-04-01 22:24:34 -0600

    Se realiza peticion a la api

--- gifs-app/src/app/gifs/services/gifs.service.ts
+++ gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,15 @@
+
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
+
   private apiKey:string = 'K77knKxTMbWDSTCpc4enPVPEaSh50WzS';
+  private serviceUrl:string = 'https://api.giphy.com/v1/gifs';
+
   private _tagsHistory: string[] = [];
-  constructor() { }
+  constructor( private http:HttpClient) { }
   get tagsHistory(){
     return [...this._tagsHistory];
   }
@@ -16,11 +21,31 @@ export class GifsService {
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.splice(0,10);
   }
-  public searchTag(tag: string):void{
+  searchTag(tag: string):void{
     if(tag.length === 0) return;
     this.organizeHistory(tag);
     //this._tagsHistory.unshift(tag);
-    console.log(this.tagsHistory);
+    //console.log(this.tagsHistory);
+    /*
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=K77knKxTMbWDSTCpc4enPVPEaSh50WzS&q=valorant&limit=10')
+    .then( resp => resp.json())
+    .then( data => console.log(data));
+    */
+    /*
+    const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=K77knKxTMbWDSTCpc4enPVPEaSh50WzS&q=valorant&limit=10');
+    const data = await resp.json();
+    console.log(data);
+    */
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag);
+
+    this.http.get(`${this.serviceUrl}/search`, { params })
+    .subscribe( resp => {
+      console.log(resp);
+
+    });
   }
 
 }
